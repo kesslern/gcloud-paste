0342ebc539839ad02a0eff506627b41a653fa24c
Nathan Kessler <nathant93@gmail.com>
2020-12-13 21:37:04 -0500

    build

---
+++ build/index.js
@@ -0,0 +1,21 @@
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+const bucket = storage.bucket('pastes.pokecenter.xyz');
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+    let file = bucket.file('testfile.html');
+    file.save('test 123 aoeu',
+        ((error) => {
+            error ? reject(error) : resolve()
+        })
+    );
+
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
